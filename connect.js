809d1fce6c20dcdabc2501d430f1c4467609dd6a
Shawn Volpe <svolpe43@gmail.com>
2016-02-01 19:58:49 -0500

    Ability to set instance health state if it is inside a ASG

--- connect.js
+++ connect.js
@@ -26,7 +26,7 @@ INSTANCES = "INSTANCES";
 var status = STACKS; 
 var stacks = [];
 var resources = [];
-var ip = -1;
+var instance = null;
 var lookup = settings.lookup;
 var key_path = settings.key_path;
 var ssh_user = settings.ssh_user;
@@ -44,14 +44,25 @@ function start(){
 
 	process.stdin.on('data', function (text) {
 
-		if (text === 'stacks\n')
+		// there may be a new line at the end of the line
+		var text = text.replace("\n", "");
+
+		if (text === 'stacks')
 		 	list_stacks();
 
-		if(text === 'ssh\n')
-			if(ip != -1)
-				ssh(ip);
+		if(text === 'ssh'){
+			if(instance != null)
+				ssh(instance);
 			else
 				console.log("You must select an EC2 instance first.");
+		}
+
+		if(text.indexOf("health=") == 0){
+			if(instance != -1)
+				set_instance_health(instance, text.substring(7));
+			else
+				console.log("You must select an EC2 instance first.");
+		}
 
 		var index = Number(text);
 		if(!isNaN(index)){
@@ -64,13 +75,17 @@ function start(){
 			}
 		}
 
-		if (text === 'quit\n')
+		if (text === 'quit')
 		 	process.exit();
 	});
 }
 
 // lists the stacks in the current AWS account
 function list_stacks(){
+
+	// reset the ip so that there is no instance selected
+	ip = -1;
+
 	status = STACKS;
 	aws_cf.listStacks({
 		StackStatusFilter: ['CREATE_COMPLETE', 'UPDATE_COMPLETE']
@@ -113,17 +128,6 @@ function select_resource(type, res_id){
 	}
 }
 
-// initiate a ssh connection with the instance in a different tab, then exit
-function ssh(ip){0
-	var command = "osascript -e 'tell application \"Terminal\" to activate' -e 'tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down' -e 'tell application \"Terminal\" to do script ";
-	command += "\"ssh -o StrictHostKeyChecking=no -i " + key_path + " " + ssh_user + "@" + ip;
-	command += "\"";
-	command += " in selected tab of the front window'";
-
-	// execute ssh
-	var ssh = exec(command, function(){});
-}
-
 // ensure to write the correct amount of spaces after the resource name
 function name_padding(string){
 	if(string === undefined){
@@ -145,6 +149,44 @@ function num_padding(num){
 	for(var i = 0; i < padding; i++)
 		out += " ";
 	return out + " |  ";
+}
+
+/*
+
+Commands
+
+*/
+
+// initiate a ssh connection with the instance in a different tab, then exit
+function ssh(instance){0
+	var command = "osascript -e 'tell application \"Terminal\" to activate' -e 'tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down' -e 'tell application \"Terminal\" to do script ";
+	command += "\"echo " + instance.id + " && ssh -o StrictHostKeyChecking=no -i " + key_path + " " + ssh_user + "@" + instance.ip;
+	command += "\"";
+	command += " in selected tab of the front window'";
+
+	// execute ssh
+	var ssh = exec(command, function(){});
+}
+
+function set_instance_health(instance, health){
+
+	console.log("$$$" + health + "$$$");
+
+	if(health !== "Unhealthy" && health !== "Healthy"){
+		console.log("That's not a good instance health state.");
+		return;
+	}
+
+	var params = {
+		HealthStatus: health,
+		InstanceId: instance.id,
+	};
+	aws_asg.setInstanceHealth(params, function(err, data) {
+		if (err) console.log(err, err.stack);
+		else{
+			select_resource("AWS::EC2::Instance", instance.id);
+		}
+	});
 }
 
 /*
@@ -185,10 +227,25 @@ function ec2_instance(res_id){
 	aws_ec2.describeInstances({ InstanceIds: [res_id] }, function(err, data) {
 		if (err) console.log(err, err.stack);
 		else{
-			var instance = data.Reservations[0].Instances[0];
-			ip = instance.PublicIpAddress;
-			console.log("\nInstancesadfas")
-			console.log(instance.InstanceId + " |  " + ip);
+			var resp_instance = data.Reservations[0].Instances[0];
+			var ip;
+
+			// if the instance is shutting down the ip will be undefined
+			if(resp_instance.PublicIpAddress !== undefined){
+				// save the selected instance
+				instance = {
+					ip : resp_instance.PublicIpAddress,
+					id: resp_instance.InstanceId
+				};
+
+				ip = instance.ip;
+			}else{
+				ip = "Null IP";
+				instance = null;
+			}
+
+			console.log("\nInstance")
+			console.log(instance.id + " |  " + ip + " |  " + resp_instance.State.Name);
 		}
 	});
 }
